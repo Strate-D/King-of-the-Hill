964297b7731f94151c7c43b3b5fffdb09c3fcfb0
jur-clerkx <jur.c@live.nl>
2015-03-11 11:47:19 +0100

    And there was water!

--- King_of_the_Hill/src/kingofthehill/domain/Mysterybox.java
+++ King_of_the_Hill/src/kingofthehill/domain/Mysterybox.java
@@ -10,40 +10,56 @@ import java.util.List;
 
 /**
  * Object that contains all the info about the mystery box in the game.
+ *
  * @author Jur
  */
 public class Mysterybox {
+
     private int resourceAmount;
     private Upgrade upgrade;
     private List<Unit> units;
-    
+
     /**
      * Creates a new Mysterybox object that contains info about the mysterybox.
-     * @param resources Amount of resources in the mysterybox. May not be a negative number!
+     *
+     * @param resources Amount of resources in the mysterybox. May not be a
+     * negative number!
      * @param upgrade Optional upgrade in the mysterybox. May be null.
      * @param units Optional list of units in the mysterybox. May be null.
      */
-    public Mysterybox(int resources, Upgrade upgrade, List<Unit> units){
+    public Mysterybox(int resources, Upgrade upgrade, List<Unit> units) {
         //Check input
-        if(resources < 0){
+        if (resources < 0) {
             throw new IllegalArgumentException("Amount of resources may not be lower than 0");
         }
-        
+
         //Set fields
         this.resourceAmount = resources;
         this.upgrade = upgrade;
         this.units = units;
     }
-    
-    public int getResourceAmount(){
+
+    /**
+     * Gets the amount of resources in the mysterybox.
+     * @return The amount of resources. Always 0 or higher.
+     */
+    public int getResourceAmount() {
         return this.resourceAmount;
     }
-    
-    public Upgrade getUpgrade(){
+
+    /**
+     * Gets the upgrade in the mysterybox.
+     * @return The upgrade in the mysterybox. Can be null.
+     */
+    public Upgrade getUpgrade() {
         return this.upgrade;
     }
-    
-    public List<Unit> getUnits(){
+
+    /**
+     * Gets the units in the mysterbox
+     * @return The list of units. Can be null.
+     */
+    public List<Unit> getUnits() {
         return Collections.unmodifiableList(units);
     }
 }
