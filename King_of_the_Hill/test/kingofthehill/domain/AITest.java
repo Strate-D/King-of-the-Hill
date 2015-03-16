30936f4a1eb7385ff21897ce47e477d7a7002d86
Corpelijn <bassiecorpelijn@hotmail.com>
2015-03-16 13:15:17 +0100

    Updated UnitTests
    Updated Unittests for:
    - AI
    - Team

--- King_of_the_Hill/test/kingofthehill/domain/AITest.java
+++ King_of_the_Hill/test/kingofthehill/domain/AITest.java
@@ -16,10 +16,26 @@ public class AITest
 {
     @Test public void testGetters()
     {
+        
     }
     
     @Test public void testSetters()
     {
+         /**
+         * Test the password check
+         */
+        
+        // default value
+        IPlayer ai = new AI();
+        
+        // 1: normal value
+        Team team1 = new Team(1, null);
+        ai.setTeam(team1);
+        assertEquals(team1, ai.getTeam());
+        assertEquals(team1.getNr(), ai.getTeam().getNr());
+        
+        // 2: normal value
+        
     }
     
     @Test public void testCheckPassword()
@@ -29,8 +45,12 @@ public class AITest
          */
         
         // default value
-        
         IPlayer ai = new AI();
-        ai.checkPassword("Welkom01");
+        
+        // 1: normal value
+        assertEquals(true, ai.checkPassword("Welkom01"));
+        
+        // 2: another normal value
+        assertEquals(true, ai.checkPassword("Dit is een test wachtwoord!"));
     }
 }
--- King_of_the_Hill/test/kingofthehill/domain/TeamTest.java
+++ King_of_the_Hill/test/kingofthehill/domain/TeamTest.java
@@ -5,6 +5,8 @@
  */
 package kingofthehill.domain;
 
+import java.awt.List;
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,5 +18,83 @@ public class TeamTest
 {
     @Test public void testGetters()
     {
+        /**
+         * Test the getters by creating some classes and reading the values
+         */
+        
+        // default value
+        IPlayer player1 = new AI("computer1");
+        IPlayer player2 = new AI("computer2");
+        ArrayList<IPlayer> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        
+        
+        // 1: normal test
+        Team team1 = new Team(1, players);
+        assertEquals(players, team1.getPlayers());
+        assertEquals(1, team1.getNr());
+        assertEquals(player1, team1.getPlayers().get(0));
+        assertEquals(player2, team1.getPlayers().get(1));
+        
+        // 2: fail test
+        try
+        {
+            Team team2 = new Team(2, null);
+            fail("List of players can not be null, can be empty");
+        }
+        catch (IllegalArgumentException ecx)
+        {
+        }
+            
+        // 3: fail test
+        try
+        {
+            Team team3 = new Team(-3, players);
+            fail ("Teamnumber cannot be less than zero!");
+        }
+        catch ( IllegalArgumentException ecx)
+        {
+        }
+        
+        // 4: normal test
+        Team team4 = new Team(4, new ArrayList<>());
+        assertEquals(4, team4.getNr());
+        assertEquals(new ArrayList<>(), team4.getPlayers());
+    }
+    
+    @Test public void testAddPlayer()
+    {
+        /**
+         * Test the method by creating an empty list and adding the players later
+         */
+        
+        // default value
+        IPlayer ai1 = new AI("Computer1");
+        IPlayer ai2 = new AI("Computer2");
+        ArrayList<IPlayer> players = new ArrayList<>();
+        players.add(ai1);
+        players.add(ai2);
+        
+        // 1: normal test
+        Team team1 = new Team(1, new ArrayList<>());
+        team1.addPlayer(ai1);
+        team1.addPlayer(ai2);
+        assertEquals(ai1, team1.getPlayers().get(0));
+        assertEquals(ai2, team1.getPlayers().get(1));
+        assertEquals(players, team1.getPlayers());
+        
+        // 2: fail test
+        Team team2 = new Team(2, new ArrayList<>());
+        team2.addPlayer(ai1);
+        team2.addPlayer(ai2);
+        team2.addPlayer(ai1);
+        assertEquals(2, team2.getPlayers().size());
+        assertEquals(ai1, team2.getPlayers().get(0));
+        assertEquals(ai2, team2.getPlayers().get(1));
+        
+        // 3: fail test
+        assertEquals(false, team2.addPlayer(null));
+        
     }
 }
