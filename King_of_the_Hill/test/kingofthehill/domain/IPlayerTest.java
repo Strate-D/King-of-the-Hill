6cdaa06d072b6df3625c5a97c00d2432623e0081
Dennis Biesters <d.biesters@student.fontys.nl>
2015-03-16 14:22:41 +0100

    IPlayer unittest

--- King_of_the_Hill/test/kingofthehill/domain/IPlayerTest.java
+++ King_of_the_Hill/test/kingofthehill/domain/IPlayerTest.java
@@ -5,6 +5,7 @@
  */
 package kingofthehill.domain;
 
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.Before;
@@ -17,21 +18,109 @@ public class IPlayerTest
 {   
    @Test public void testGetters()
    {
+        /**
+         * Test the getters by creating some objects and check their values
+        */
+       
+        // default value
         String name = "Bastiaan";
         int exp = 10;
         
+        // 1: normal player test
         IPlayer player = new Player(name, exp);
         assertEquals(name, player.getName());
         assertEquals(exp, player.getExp());
         assertEquals(0, player.getScore());
-        assertEquals(100, player.getMoney());
-        assertNull(player.getUpgrades());
+        assertTrue(player.getUpgrades().isEmpty());
         assertNull(player.getTeam());
+        assertEquals(100, player.getMoney());
         assertNull(player.getBase());
+        
+        // 2: normal ai test
+        IPlayer ai = new AI(name);
+        assertEquals(name, ai.getName());
+        assertEquals(12, ai.getExp());
+        assertEquals(0, ai.getScore());
+        assertTrue(ai.getUpgrades().isEmpty());
+        assertNull(ai.getTeam());
+        assertEquals(100, ai.getMoney());
+        assertNull(ai.getBase());
+        
+        // 3: fail test
+        try{
+            player = new Player("", 1);
+            fail("Player without a name is created");
+        }
+        catch(IllegalArgumentException exc){}
+        
+        // 4: fail test
+        try{
+            player = new Player(name, -1);
+            fail("Player without exp is created");
+        }
+        catch(IllegalArgumentException exc){}
+        
+        // 5: fail test
+        try{
+            player = new AI("");
+            fail("Ai without a name is created");
+        }
+        catch(IllegalArgumentException exc){}
    }
    
    @Test public void testSetters()
    {
-       IPlayer player = new Player("Rickske", 10);
+       /**
+        * Test the setters by creating some objects and check their values
+       */
+       
+       // default value
+       String name = "Rickske";
+       IPlayer player = new Player(name, 10);
+       Base base = new Base(player);
+       ArrayList<IPlayer> players = new ArrayList<>();
+       players.add(player);   
+       Team team = new Team(1, players);
+       
+       // 1: normal player test
+       player.setBase(base);
+       assertEquals(base, player.getBase());
+       player.setTeam(team);
+       assertEquals(team, player.getTeam());
+       
+       // 2: normal ai test
+       IPlayer ai = new AI(name);
+       ai.setBase(base);
+       assertEquals(base, ai.getBase());
+       ai.setTeam(team);
+       assertEquals(team, ai.getTeam());
+   }
+   
+   @Test public void testAddPayMoney()
+   {
+        /**
+        * Test the setters by creating some objects and check their values
+        */
+       
+       // default value
+       String name = "Berta";
+       
+       // 1: normal player test
+       IPlayer player = new Player(name, 10);
+       player.addMoney(10);
+       assertEquals(110, player.getMoney());
+       assertTrue(player.payMoney(10));
+       assertFalse(player.payMoney(101));
+       assertTrue(player.payMoney(100));
+       assertFalse(player.payMoney(1));
+       
+       // 2: normal ai test
+       IPlayer ai = new AI(name);
+       ai.addMoney(99);
+       assertEquals(199, ai.getMoney());
+       assertTrue(ai.payMoney(10));
+       assertFalse(ai.payMoney(190));
+       assertTrue(ai.payMoney(100));
+       assertFalse(ai.payMoney(91)); 
    }
 }
