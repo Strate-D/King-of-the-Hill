2944edf80cbb9520b858d9563b086562a9c2f5eb
Dennis Biesters <d.biesters@student.fontys.nl>
2015-03-18 11:11:53 +0100

    Unittest lane

--- King_of_the_Hill/test/kingofthehill/domain/LaneTest.java
+++ King_of_the_Hill/test/kingofthehill/domain/LaneTest.java
@@ -10,11 +10,77 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Bas
+ * @author Dennis
  */
 public class LaneTest 
 {
     @Test public void testGetters()
     {
+        /**
+        * Test the getters by creating some objects and check their values
+        */
+       
+        // default value
+        Base base1 = new Base(new Player("Henkie", 1337));
+        Base base2 = new Base(new Player("Frenkie", 2123));
+        Lane l = new Lane(base1, base2);
+        
+        // 1: normal test
+        assertEquals(base1, l.getBaseEnd1());
+        assertEquals(base2, l.getBaseEnd2());
+        assertEquals(0, l.getUnits().size());
+        
+        // 2: fail test
+        Lane l2 = new Lane(null, null);
+        assertNotNull(l2.getBaseEnd1());
+        assertNotNull(l2.getBaseEnd2());
+    }
+    
+    @Test public void testAddUnit()
+    {
+        /**
+        * Test adding a unit to a lane and check their values
+        */
+        
+        // default value
+        IPlayer player1 = new AI("Jurske");
+        Base base1 = new Base(player1);
+        Base base2 = new Base(new Player("Baske", 2123));
+        Lane l = new Lane(base1, base2);
+        Unit u = new Defence(12, 12, 12, 12, player1);
+        
+        // 1: normal test
+        assertEquals(0, l.getUnits().size());
+        l.addUnit(u);
+        assertEquals(1, l.getUnits().size());
+        
+        // 2; fail test
+        l.addUnit(null);
+        assertEquals(1, l.getUnits().size());
+    }
+    
+    @Test public void testRemoveUnit()
+    {
+        /**
+        * Test removing a unit to a lane and check their values
+        */
+        
+        // default value
+        IPlayer player1 = new AI("Koek");
+        Base base1 = new Base(player1);
+        Base base2 = new Base(new Player("Dennis", 2123));
+        Lane l = new Lane(base1, base2);
+        Unit u = new Defence(12, 12, 12, 12, player1);
+        l.addUnit(u);
+        l.addUnit(u);
+        
+        // 1: normal test
+        l.removeUnit(u);
+        assertEquals(1, l.getUnits().size());
+        
+        // 2: fail test
+        l.addUnit(u);
+        l.removeUnit(null);
+        assertEquals(1, l.getUnits().size());
     }
 }
