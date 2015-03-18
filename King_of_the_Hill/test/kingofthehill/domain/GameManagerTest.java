320786c55a32234124e9f74f1054ce441938e1d8
Corpelijn <bassiecorpelijn@hotmail.com>
2015-03-18 11:30:05 +0100

    Updated UnitTest
    Updated Unittests for:
    - GameManager

--- King_of_the_Hill/test/kingofthehill/domain/GameManagerTest.java
+++ King_of_the_Hill/test/kingofthehill/domain/GameManagerTest.java
@@ -16,5 +16,81 @@ public class GameManagerTest
 {
     @Test public void testGetters()
     {
+        /**
+         * Test the constructor and the getPlayers() method
+         */
+        
+        // normal test
+        GameManager gm = new GameManager(new Player("Bas", 100));
+        assertEquals(new Player("Bas", 100), gm.getPlayers().get(0));
+        assertEquals(new AI("AI1"), gm.getPlayers().get(1));
+        assertEquals(new AI("AI2"), gm.getPlayers().get(2));
+        assertEquals(new AI("AI3"), gm.getPlayers().get(3));
+        
+        assertEquals(1, gm.getPlayers().get(0).getTeam().getNr());
+        assertEquals(2, gm.getPlayers().get(1).getTeam().getNr());
+        assertEquals(1, gm.getPlayers().get(2).getTeam().getNr());
+        assertEquals(2, gm.getPlayers().get(3).getTeam().getNr());
+        
+        // normal test
+        gm = new GameManager(new AI("AI0"));
+        assertEquals(new AI("AI0"), gm.getPlayers().get(0));
+        assertEquals(1, gm.getPlayers().get(0).getTeam().getNr());
+        
+        // fail test
+        try
+        {
+            gm = new GameManager(null);
+        }
+        catch (NullPointerException ecx) {}
+    }
+    
+    @Test public void testGenerateMysterybox()
+    {
+        fail("No javadoc written!");
+    }
+    
+    @Test public void testPlaceUnitAtLane()
+    {
+        // create values
+        IPlayer ai = new AI("AI1");
+        Unit def = new Defence(10, 10, 10, 10, ai);
+        GameManager gm = new GameManager(new Player("Bas", 10));
+        
+        // normal test
+        assertFalse(gm.placeUnitAtLane(ai, def, 0, 100));
+        
+        // normal test
+        assertTrue(gm.placeUnitAtLane(ai, def, 1, 200));
+        
+        // fail test : null value
+        try
+        {
+            gm.placeUnitAtLane(null, null, 2, 10);
+            fail("Values cannot be null");
+        }
+        catch (NullPointerException ecx) {}
+    }
+    
+    @Test public void testPlaceUnitAtBase()
+    {
+        // create values
+        IPlayer ai = new AI("AI1");
+        Unit def = new Defence(10, 10, 10, 10, ai);
+        GameManager gm = new GameManager(new Player("Bas", 10));
+        
+        // normal test
+        assertFalse(gm.placeUnitAtBase(ai, def, 1, 100));
+        
+        // normal test
+        assertTrue(gm.placeUnitAtBase(ai, def, 1, 200));
+        
+        // fail test : null value
+        try
+        {
+            gm.placeUnitAtBase(null, null, 2, 10);
+            fail("Values cannot be null");
+        }
+        catch (NullPointerException ecx) {}
     }
 }
