e033962748d0e6dbf08cd5d059e6b304ef66168f
Jur Clerkx <jur.c@live.nl>
2015-06-24 12:20:22 +0200

    Fixed house bug

--- King_of_the_Hill/src/kingofthehill/UI/FXMLMultiPlayerViewController.java
+++ King_of_the_Hill/src/kingofthehill/UI/FXMLMultiPlayerViewController.java
@@ -648,7 +648,7 @@ public class FXMLMultiPlayerViewController implements Initializable {
                 && lastRealMousePosy >= 90 && lastRealMousePosy <= 120 && defenceCooldown <= 0) {
             selectedUnit = UnitInfo.getDefenceUnit(gameInfo.getPlayers().get(0));
         } else if (lastRealMousePosx >= 125 && lastRealMousePosx <= 155
-                && lastRealMousePosy >= 125 && lastRealMousePosy <= 155 && defenceCooldown <= 0) {
+                && lastRealMousePosy >= 125 && lastRealMousePosy <= 155 && resourceCooldown <= 0) {
             selectedUnit = UnitInfo.getResourceUnit(gameInfo.getPlayers().get(0));
         }
         /**
